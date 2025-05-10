022654261cd108a31eddf9765203a1e6c5b8f088
Flopster101 <nahuelgomez329@gmail.com>
2025-05-09 23:49:25 -0300

    build: log to build.log
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,14 +5,16 @@ COMMON_ARGS="-j$(nproc --all) -C $(pwd) O=$(pwd)/${OUT_DIR} ARCH=arm CROSS_COMPI
 
 export PATH=$(realpath ../android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9/bin/):$PATH
 
+rm -f build.log
+
 if [[ "$1" == "-c" ]]; then
     echo "Cleaning build directory..."
     [ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR}
 fi
 mkdir -p ${OUT_DIR}
 
-make ${COMMON_ARGS} msm8937_sec_defconfig VARIANT_DEFCONFIG=msm8937_sec_gta2slte_sea_open_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make ${COMMON_ARGS}
+make ${COMMON_ARGS} msm8937_sec_defconfig VARIANT_DEFCONFIG=msm8937_sec_gta2slte_sea_open_defconfig SELINUX_DEFCONFIG=selinux_defconfig | tee -a build.log
+make ${COMMON_ARGS} | tee -a build.log
 
 cp ${OUT_DIR}/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
 
