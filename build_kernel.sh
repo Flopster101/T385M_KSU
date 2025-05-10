93d45a481a958829e709611126218d7c00a9f4e9
Flopster101 <nahuelgomez329@gmail.com>
2025-05-09 23:57:53 -0300

    build: simplify out dir usage
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OUT_DIR=out
-COMMON_ARGS="-j$(nproc --all) -C $(pwd) O=$(pwd)/${OUT_DIR} ARCH=arm CROSS_COMPILE=arm-linux-androideabi-"
+COMMON_ARGS="-j$(nproc --all) O=${OUT_DIR} ARCH=arm CROSS_COMPILE=arm-linux-androideabi-"
 
 export PATH=$(realpath ../android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9/bin/):$PATH
 
@@ -11,7 +11,6 @@ if [[ "$1" == "-c" ]]; then
     echo "Cleaning build directory..."
     [ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR}
 fi
-mkdir -p ${OUT_DIR}
 
 make ${COMMON_ARGS} msm8937_sec_defconfig VARIANT_DEFCONFIG=msm8937_sec_gta2slte_sea_open_defconfig SELINUX_DEFCONFIG=selinux_defconfig | tee -a build.log
 make ${COMMON_ARGS} | tee -a build.log
